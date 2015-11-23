5ef9c668b8121b66617c859e576e2bd9dfc25736
Daniel Warner <danywarner@gmail.com>
2015-11-22 23:28:49 -0500

    not showing alert on start

--- www/js/index.js
+++ www/js/index.js
@@ -59,7 +59,7 @@ function fillGrid(){
         loaded = true;
     screenWidth = screen.width;
     screenHeight = screen.height;
-    alert(screenWidth+ " x "+screenHeight)
+    //alert(screenWidth+ " x "+screenHeight)
     switch(screenWidth){
         case 320: //iphone 4s, 5, 5s
         case 360:  //nexus 5,...
